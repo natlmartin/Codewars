f3863c0f6625384e7df32d79a9d92e4e8accb6c4
natlmartin <nat.louise.martin@gmail.com>
2023-07-09 16:24:14 +0100

    Add kyu7_simple_pig_latin.py
    Add kyu7_simple_pig_latin.py

---
+++ kyu7_simple_pig_latin.py
@@ -0,0 +1,22 @@
+text = "hello world, how are you?"
+
+
+def pig_it(text):
+    pig_it_list = []
+    text_split = text.split(" ")
+
+    for word in text_split:
+        # if word is not punctuation
+        if word.isalpha():
+            # save letters after index 0 + index 0 + "ay" to new_word
+            new_word = word[1:] + word[0] + "ay"
+            pig_it_list.append(new_word)
+        else:
+            pig_it_list.append(word)
+
+    pig_it_string = " ".join(pig_it_list)
+    return pig_it_string
+
+
+pig_it(text)
+
